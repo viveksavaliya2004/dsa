fb876fca2319b567db20c5e7bec8702817d2e18d
Vivek Savaliya <155444580+viveksavaliya2004@users.noreply.github.com>
2026-04-02 10:18:56 +0530

    Time: 65 ms (94.21%), Space: 131.5 MB (29%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -4,32 +4,26 @@ public:
         
         int n = profits.size();
 
-        // Step 1: store (capital, profit)
+        
         vector<pair<int,int>> projects;
         for(int i = 0; i < n; i++){
             projects.push_back({capital[i], profits[i]});
         }
 
-        // Step 2: sort by capital
         sort(projects.begin(), projects.end());
 
-        // Step 3: max heap for profits
+        
         priority_queue<int> pq;
 
         int i = 0;
 
         for(int j = 0; j < k; j++){
 
-            // add all affordable projects
             while(i < n && projects[i].first <= w){
                 pq.push(projects[i].second);
                 i++;
             }
-
-            // if no project available
             if(pq.empty()) break;
-
-            // pick max profit
             w += pq.top();
             pq.pop();
         }
