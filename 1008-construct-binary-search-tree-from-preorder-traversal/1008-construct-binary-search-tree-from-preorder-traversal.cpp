dfb5784023f95b19dbfda0761dd370e841667952
Vivek Savaliya <155444580+viveksavaliya2004@users.noreply.github.com>
2026-06-12 12:48:41 +0530

    Time: 0 ms (100%), Space: 16.4 MB (45.3%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* build(vector<int>& preorder, int& i, int bound) {
+        if (i == preorder.size() || preorder[i] > bound)
+            return NULL;
+
+        TreeNode* root = new TreeNode(preorder[i++]);
+
+        root->left = build(preorder, i, root->val);
+        root->right = build(preorder, i, bound);
+
+        return root;
+    }
+
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i = 0;
+        return build(preorder, i, INT_MAX);
+    }
+};
