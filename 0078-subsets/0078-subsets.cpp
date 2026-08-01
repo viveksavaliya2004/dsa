afe74585c0cee092e5970f9c2e4ed0e7249f9aec
Vivek Savaliya <155444580+viveksavaliya2004@users.noreply.github.com>
2026-08-01 16:48:05 +0530

    Time: 3 ms (27.31%), Space: 15.4 MB (15.14%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
 
-        int n = nums.size();
-        int totalSubsets = 1 << n;      // 2^n
-
-        vector<vector<int>> ans;
-
-        for (int mask = 0; mask < totalSubsets; mask++) {
-
-            vector<int> subset;
-
-            for (int i = 0; i < n; i++) {
-
-                // Check if ith bit is set
-                if (mask & (1 << i)) {
-                    subset.push_back(nums[i]);
-                }
-            }
-
-            ans.push_back(subset);
+    void fun(int idx ,vector<int> nums ,vector<int>&vi , vector<vector<int>>&ans){
+        if(idx == nums.size()){
+            ans.push_back(vi);
+            return ;
         }
 
+        vi.push_back(nums[idx]);
+        fun(idx+1 , nums , vi , ans);
+
+        vi.pop_back();
+        fun(idx+1 , nums , vi , ans);
+    }
+    
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>vi;
+        vector<vector<int>>ans;
+        int idx = 0;
+        fun(idx , nums , vi , ans );
         return ans;
     }
 };
