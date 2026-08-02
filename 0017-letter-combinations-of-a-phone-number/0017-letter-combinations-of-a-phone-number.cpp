be3a16b5e20fc55fcc564c0f1e91c5070382d87f
Vivek Savaliya <155444580+viveksavaliya2004@users.noreply.github.com>
2026-08-02 14:57:37 +0530

    Time: 0 ms (100%), Space: 9.6 MB (20.75%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-
-        if (digits.empty())
-            return {};
-
-        vector<string> mapp = {
-            "", "", "abc", "def", "ghi",
-            "jkl", "mno", "pqrs", "tuv", "wxyz"
-        };
-
-        vector<string> r;
-        r.push_back("");   
-        for (char d : digits) {
-            vector<string> temp;
-
-            for (string s : r) {
-                for (char ch : mapp[d - '0']) {
-                    temp.push_back(s + ch);
-                }
-            }
-
-            r = temp;
+    vector<string>ans;
+    void slove(int idx , string &digits , string &temp , unordered_map<char , string>&mp ){
+        if(idx >= digits.size()){
+            ans.push_back(temp);
+            return;
         }
-
-        return r;
+        char ch = digits[idx];
+        string str = mp[ch];
+        for(int i=0;i<str.size();i++){
+            temp.push_back(str[i]);
+            slove(idx+1 , digits , temp , mp);
+            temp.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        string temp = "";
+        unordered_map<char , string>mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        
+        slove(0,digits , temp , mp);
+        return ans;
     }
 };
